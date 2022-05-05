84e3a2f4001bc2b2393abb9c2c32ba477fb47d3a
Zergarus <47299260+Zergarus@users.noreply.github.com>
2022-05-05 17:05:32 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+    $('.owl-one').owlCarousel({
+        items: 4,
+        loop:true,
+        margin:10,
+        nav:true,
+        navText : ["<i class='arrow-left'><img src='images/next 24.png' /></i>","<i class='arrow-right'><img src='images/next 23.png' /></i>"],
+        responsive:{ //Адаптация в зависимости от разрешения экрана
+            0:{
+                items:1,
+                margin:-53,
+                nav:false,
+            },
+            900:{
+                items:4
+            },
+        },
+    });
+
+    $('.owl-two').owlCarousel({
+        items: 6,
+        loop:true,
+        margin:10,
+        nav:true,
+        navText : ["<i class='arrow-left'><img src='images/next 24.png' /></i>","<i class='arrow-right'><img src='images/next 23.png' /></i>"]
+
+    });
+    $('.owl-three').owlCarousel({
+        items: 1,
+        loop:true,
+        margin:10,
+        nav:true,
+        navText : ["<i class='arrow-left'><img src='images/next 24.png' /></i>","<i class='arrow-right'><img src='images/next 23.png' /></i>"]
+
+    });
+    $('.owl-four').owlCarousel({
+        items: 1,
+        loop:true,
+        margin:0,
+        nav:true,
+        navText : ["<i class='arrow-left'><img src='images/next 2.png' /></i>","<i class='arrow-right'><img src='images/next 2(1).png' /></i>"]
+
+    });
+    $('.owl-four-formobile').owlCarousel({
+        items: 1,
+        loop:true,
+        margin:0,
+        nav:true,
+    });
+    $(".button_play").on("click", function(){
+        $('#video')[0].play();
+        $('.button_play').hide();
+        });
+        $("#video").on("click", function(){
+        $('#video')[0].pause();
+        $('.button_play').show();
+        });
+    $('.button_bottom').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 1200);
+        return false;
+    });
+    $(".hideThing").slideUp(1);
+    $(".roll").click(function(){
+        $(".hideThing").slideToggle();
+        if ( $( ".roll" ).text() == "Развернуть...") {
+            $( ".roll" ).text( "Свернуть..." );
+          } else {
+            $( ".roll" ).text( "Развернуть..." );
+          };
+    });
+});
